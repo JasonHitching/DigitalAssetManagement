bede9e60e1309f48499fdfbb870694cdd1527ec4
Jason Hitching <jasonhitching1994@gmail.com>
2024-11-13 19:17:09 +0000

    added service interface

--- Services/Abstractions/IBlobStorageService.cs
+++ Services/Abstractions/IBlobStorageService.cs
@@ -1,10 +1,7 @@
-// Generate an interface for BlobStorageService.cs child class
+namespace Services.Abstractions;
 
-namespace Services.Abstractions
+public interface IBlobStorageService
 {
-    public interface IBlobStorageService
-    {
-        Task<string> UploadImageAsync(string fileName, Stream stream);
-        Task<Stream> DownloadImageAsync(string fileName);
-    }
+    Task<string> UploadImageAsync(string fileName, Stream stream);
+    Task<Stream> DownloadImageAsync(string fileName);
 }
