322427125e6de55bd1a00175c80bba33366f6bb9
Jason Hitching <jasonhitching1994@gmail.com>
2024-11-13 21:14:54 +0000

    Further blob client and app settings config

---
+++ DigitalAssetManager/Services/Abstractions/IBlobStorageService.cs
@@ -0,0 +1,8 @@
+namespace Services.Abstractions
+{
+    public interface IBlobStorageService
+    {
+        Task<string> UploadImageAsync(string fileName, Stream stream);
+        Task<Stream> DownloadImageAsync(string fileName);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,28 @@
+<<<<<<< Updated upstream:Program.cs
 using DigitalAssetManager.Components;
+=======
+using Services.Implementations;
+using Services.Abstractions;
+>>>>>>> Stashed changes:DigitalAssetManager/Program.cs
 
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+
+configuration.AddJsonFile("appsettings.Development.json", optional: true, reloadOnChange: true);
+configuration.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
 
 // Add services to the container.
+<<<<<<< Updated upstream:Program.cs
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+=======
+builder.Services.AddRazorPages();
+builder.Services.AddSingleton<IBlobStorageService, BlobStorageService>();
+>>>>>>> Stashed changes:DigitalAssetManager/Program.cs
 
 var app = builder.Build();
+
+var blobstorageService = app.Services.GetRequiredService<IBlobStorageService>();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
